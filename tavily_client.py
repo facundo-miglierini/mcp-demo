1d81be32e5fd3f1f842bc5df247d883ac73e3913
Facundo Miglierini <facundo.miglierini@snoopconsulting.com>
2025-04-22 16:58:14 -0300

    First steps

---
+++ tavily_client.py
@@ -0,0 +1,38 @@
+# Create server parameters for stdio connection
+from mcp import ClientSession, StdioServerParameters
+
+from mcp.client.stdio import stdio_client
+
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+from models import model
+
+server_params = StdioServerParameters(
+    command="node",
+    args=["/home/facundo/tavily-mcp/build/index.js"]
+)
+
+async def run():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            # Initialize the connection
+            await session.initialize()
+
+            # Get tools
+            tools = await load_mcp_tools(session)
+
+            # Get prompt
+            prompt = "You are a Tavily agent."
+
+            # Create and run the agent
+            agent = create_react_agent(model, tools, prompt=prompt)
+            #agent_response = await agent.ainvoke({"messages": "Cuándo se elige el nuevo papa?"})
+            agent_response = await agent.ainvoke({"messages": "Qué contiene el siguiente link? https://github.com/varunneal/spotify-mcp"})
+            print(f"AGENT RESPONSE: {agent_response['messages'][-1].content}")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
