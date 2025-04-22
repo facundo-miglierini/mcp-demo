201cb2a389d2f37b881eee2f820bbfb3b3a77908
Facundo Miglierini <facundo.miglierini@snoopconsulting.com>
2025-04-22 17:19:20 -0300

    Added streamlit

---
+++ src/streamlit.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import time
+
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+
+from tavily_client import agent
+
+import nest_asyncio
+nest_asyncio.apply()
+
+st.title("MCP agent")
+
+if "messages" not in st.session_state:
+    # default initial message to render in message state
+    st.session_state["messages"] = [AIMessage("¿En qué puedo ayudarte?")]
+
+# Loop through all messages in the session state and render them as a chat on every st.refresh mech
+for msg in st.session_state.messages:
+    # https://docs.streamlit.io/develop/api-reference/chat/st.chat_message
+    # we store them as AIMessage and HumanMessage as its easier to send to LangGraph
+    if type(msg) == AIMessage:
+        st.chat_message("assistant").write(msg.content)
+
+    if type(msg) == HumanMessage:
+        st.chat_message("user").write(msg.content)
+
+async def run():
+    if prompt := st.chat_input():
+        user_prompt = HumanMessage(content=prompt)
+        st.session_state.messages.append(user_prompt)
+        st.chat_message("user").write(prompt)
+
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+            response = await agent.invoke(prompt, [st_callback], "1")
+
+            message_placeholder = st.empty()
+            full_response = ""
+            for chunk in response.split():
+                full_response += chunk + " "
+                time.sleep(0.05)
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+            st.session_state.messages.append(AIMessage(content=response))
+
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
--- src/tavily_client.py
+++ src/tavily_client.py
@@ -1,4 +1,6 @@
-# Create server parameters for stdio connection
+from langchain_core.runnables.config import RunnableConfig
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.graph import CompiledGraph
 from mcp import ClientSession, StdioServerParameters
 
 from mcp.client.stdio import stdio_client
@@ -13,6 +15,44 @@ server_params = StdioServerParameters(
     args=["/home/facundo/tavily-mcp/build/index.js"]
 )
 
+class TavilyAgent:
+    def __init__(self, server_params, checkpointer):
+        self.server_params = server_params
+        self.checkpointer = checkpointer
+        self.session = None
+        self.agent: CompiledGraph|None = None
+
+
+    async def invoke(self, query: str, callback, thread_id: str|None = None) -> str:
+        async with stdio_client(self.server_params) as (read, write):
+            async with ClientSession(read, write) as session:
+
+                config = RunnableConfig(
+                    callbacks=callback,
+                    recursion_limit=30,
+                    configurable={ 
+                        "thread_id": thread_id
+                    }
+                )
+
+                # Initialize the connection
+                self.session = session
+                await self.session.initialize()
+
+                # Get tools
+                tools = await load_mcp_tools(self.session)
+
+                # Create and run the agent
+                self.agent = create_react_agent(model, tools, checkpointer=self.checkpointer)
+                agent_response = await self.agent.ainvoke({"messages": query}, config=config)
+                return agent_response['messages'][-1].content
+
+
+checkpointer = MemorySaver()
+agent = TavilyAgent(server_params, checkpointer)
+
+
+# TESTING IN CONSOLE
 async def run():
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
@@ -24,9 +64,13 @@ async def run():
 
             # Create and run the agent
             agent = create_react_agent(model, tools)
-            #agent_response = await agent.ainvoke({"messages": "Cuándo se elige el nuevo papa?"})
-            agent_response = await agent.ainvoke({"messages": "Qué contiene el siguiente link? https://github.com/varunneal/spotify-mcp"})
-            print(f"AGENT RESPONSE: {agent_response['messages'][-1].content}")
+
+            msg = ""
+
+            while msg != "quit":
+                msg = input("USER PROMPT (quit): ")
+                agent_response = await agent.ainvoke({"messages": "Qué contiene el siguiente link? https://github.com/varunneal/spotify-mcp"})
+                print(f"AGENT RESPONSE: {agent_response['messages'][-1].content}")
 
 
 if __name__ == "__main__":
